6fa37a7c156d30f04cb90ec88b170487542f1362
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2020-07-03 14:45:34 -0400

    Spring model

---
+++ proj/BugTracker/BugTracker/Models/Sprint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BugTracker.Models
+{
+    public class Sprint
+    {
+        public int ID { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public string Goals { get; set; }
+        public string Retrospective { get; set; }
+        public int Points { get; set; }
+        public int PointsEliminated { get; set; }
+        public ICollection<Ticket> Assignments { get; set; }
+    }
+}
