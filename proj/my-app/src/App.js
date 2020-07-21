b8f0b78b1798aba789d133ce784a5d9a2f575103
Ahmed Maazouzi <ahmaazouzi@mix.wvu.edu>
2020-07-21 04:50:54 -0400

    Successfully routed; RIP MB

--- proj/my-app/src/App.js
+++ proj/my-app/src/App.js
@@ -1,9 +1,36 @@
-import React, { Component, useState} from 'react';
+import React, { Component, useState } from 'react';
 
 import Sidebar from "./components/layout/sidebar";
 import Header from "./components/layout/header";
 import Body from "./components/layout/body";
 import { Row, Container, Col, Spinner, Button, Modal } from "react-bootstrap";
+
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
+const routes = [
+  {
+    path: "/",
+    exact: true,
+    sidebar: () => <div>home!</div>,
+    main: () => <h2>Home</h2>
+  },
+  {
+    path: "/bubblegum",
+    sidebar: () => <div>bubblegum!</div>,
+    main: () => <h2>Bubblegum</h2>
+  },
+  {
+    path: "/shoelaces",
+    sidebar: () => <div>shoelaces!</div>,
+    main: () => <h2>Shoelaces</h2>
+  }
+];
+
 
 class App extends Component {
   constructor(props) {
@@ -17,14 +44,14 @@ class App extends Component {
   }
 
   changeTicketStatus = (tickets) => {
-    this.setState({tickets: tickets });
+    this.setState({ tickets: tickets });
   }
 
-  showModal = (a) => { this.setState({showModal: true})};
-  
-  showCreateModal = () => this.setState({showModal: true});
+  showModal = (a) => { this.setState({ showModal: true }) };
 
-  addTicket = m => this.setState({tickets: [...this.state.tickets, {status: m}]})
+  showCreateModal = () => this.setState({ showModal: true });
+
+  addTicket = m => this.setState({ tickets: [...this.state.tickets, { status: m }] })
 
   componentDidMount() {
     fetch("https://localhost:5001/sprints/1")
@@ -48,7 +75,7 @@ class App extends Component {
       )
   }
 
-	render(){
+  render() {
     const { error, isLoaded } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -56,22 +83,40 @@ class App extends Component {
       return (<Spinit />)
     } else {
       return (
-        <Container fluid> 
-          <Example showModal={this.state.showModal} setState={ s => this.setState(s)} addTicket={d => this.addTicket(d)}/>
-          <Row>
-            <Sidebar showCreateModal={this.showModal}/>
-            <Col>
-              <Header onClick={this.showModal}/>
-              <Body tickets={this.state.tickets} setState={ s => this.setState(s)} showModal={a => this.showModal(a)}/>
-            </Col>
-          </Row>
-        </Container>
-        );
+        <Router>
+          <Container fluid>
+            <Example showModal={this.state.showModal} setState={s => this.setState(s)} addTicket={d => this.addTicket(d)} />
+            <Row>
+              <Sidebar showCreateModal={this.showModal} />
+              <Col>
+                <Header onClick={this.showModal} />
+                <Switch>
+                  <Route exact path="/">
+                    <Body tickets={this.state.tickets} setState={s => this.setState(s)} showModal={a => this.showModal(a)} />
+                  </Route>
+                  <Route path="/sprints">
+                    <Sprints />
+                  </Route>
+                  <Route path="/team">
+                    <Team />
+                  </Route>
+                  <Route path="/messages">
+                    <Messages />
+                  </Route>
+                  <Route path="/settings">
+                    <Settings />
+                  </Route>
+                </Switch>
+              </Col>
+            </Row>
+          </Container>
+        </Router>
+      );
     }
-	}
+  }
 }
 
-function Spinit(){
+function Spinit() {
   const spinnerStyle = {
     height: "100%",
     display: "flex",
@@ -102,8 +147,8 @@ function Example(props) {
     alert('A name was submitted: ' + status);
   }
 
-  function handleShows(){
-    props.setState({showModal: false});
+  function handleShows() {
+    props.setState({ showModal: false });
   }
 
   return (
@@ -114,25 +159,67 @@ function Example(props) {
         </Modal.Header>
         <Modal.Body>
 
-        <form onSubmit={handleSubmit}>
-        <label>
-          Name:
+          <form onSubmit={handleSubmit}>
+            <label>
+              Name:
           <input type="text" value={status} onChange={handleChange} />
-        </label>
-      </form>
+            </label>
+          </form>
 
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={handleShows}>
             Close
           </Button>
-          <Button variant="primary" onClick={() => {props.addTicket(status); handleShows()}} >
+          <Button variant="primary" onClick={() => { props.addTicket(status); handleShows() }} >
             Save Changes
           </Button>
         </Modal.Footer>
       </Modal>
     </>
   );
+}
+
+function Settings() {
+  return (
+    <div>
+      <h1>
+        My settings
+      </h1>
+      <p>Settings go here, mate!</p>
+    </div>)
+}
+
+function Sprints() {
+  return (
+    <div>
+      <h1>
+        My Sprints
+      </h1>
+      <p>Here, we fix them sprints</p>
+    </div>)
+}
+
+function Team() {
+  return (
+    <div>
+      <h1>
+        My team
+      </h1>
+      <p>Here you discipline (or get disciplined by yout team</p>
+    </div>)
+}
+
+function Messages() {
+  return (
+    <div>
+      <h1>
+        Messages
+      </h1>
+      <p>
+        Here, you sneakily see messages!
+      </p>
+    </div>)
 }
 
 
--- proj/my-app/src/components/layout/sidebar.js
+++ proj/my-app/src/components/layout/sidebar.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { Col, Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 class Sidebar extends Component {
     render() {
@@ -34,55 +35,55 @@ class Sidebar extends Component {
                 </p>
 
                 <p style={{paddingTop: "1em"}} className="text-center">
-                    <a href="http://localhost:3000" style={{color: "white"}}>
+                    <Link to="/" href="http://localhost:3000" style={{color: "white"}}>
                         <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M6.5 10.995V14.5a.5.5 0 0 1-.5.5H2a.5.5 0 0 1-.5-.5v-7a.5.5 0 0 1 .146-.354l6-6a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 .146.354v7a.5.5 0 0 1-.5.5h-4a.5.5 0 0 1-.5-.5V11c0-.25-.25-.5-.5-.5H7c-.25 0-.5.25-.5.495z"/>
                             <path fillRule="evenodd" d="M13 2.5V6l-2-2V2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5z"/>                        
                         </svg>
                         <br></br>
                         Home
-                    </a>
+                    </Link>
                 </p>
 
                 <p style={{paddingTop: "1em"}} className="text-center">
-                    <a href="http://localhost:3000" style={{color: "white"}}>
+                    <Link to="/sprints" href="http://localhost:3000" style={{color: "white"}}>
                         <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M8 15A7 7 0 1 0 8 1a7 7 0 0 0 0 14zm8-7A8 8 0 1 1 0 8a8 8 0 0 1 16 0z"/>
                             <path fillRule="evenodd" d="M7.5 3a.5.5 0 0 1 .5.5v5.21l3.248 1.856a.5.5 0 0 1-.496.868l-3.5-2A.5.5 0 0 1 7 9V3.5a.5.5 0 0 1 .5-.5z"/>                       
                         </svg>
                         <br></br>
                         Sprints
-                    </a>
+                    </Link>
                 </p>
 
                 <p style={{paddingTop: "1em"}} className="text-center">
-                    <a href="http://localhost:3000" style={{color: "white"}}>
+                    <Link to="/team" href="http://localhost:3000" style={{color: "white"}}>
                         <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1H7zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5.784 6A2.238 2.238 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.325 6.325 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1h4.216zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5z"/>                       
                         </svg>
                         <br></br>
                         Team
-                    </a>
+                    </Link>
                 </p>
 
                 <p style={{paddingTop: "1em", whiteSpace: "nowrap"}} className="text-center">
-                    <a href="http://localhost:3000" style={{color: "white"}}>
+                    <Link to="/messages" href="http://localhost:3000" style={{color: "white"}}>
                         <span><svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path d="M8 15c4.418 0 8-3.134 8-7s-3.582-7-8-7-8 3.134-8 7c0 1.76.743 3.37 1.97 4.6-.097 1.016-.417 2.13-.771 2.966-.079.186.074.394.273.362 2.256-.37 3.597-.938 4.18-1.234A9.06 9.06 0 0 0 8 15z"/>
                         </svg><span style={notificationBadge}>2</span></span>
                         <br></br>
                         Messages
-                    </a>
+                    </Link>
                 </p>
 
                 <p style={{paddingTop: "1em"}} className="text-center">
-                    <a href="http://localhost:3000" style={{color: "white"}}>
+                    <Link to="/settings" href="http://localhost:3000" style={{color: "white"}}>
                         <svg width="2em" height="2em" viewBox="0 0 16 16" className="bi bi-plus-circle-fill" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
                             <path fillRule="evenodd" d="M9.405 1.05c-.413-1.4-2.397-1.4-2.81 0l-.1.34a1.464 1.464 0 0 1-2.105.872l-.31-.17c-1.283-.698-2.686.705-1.987 1.987l.169.311c.446.82.023 1.841-.872 2.105l-.34.1c-1.4.413-1.4 2.397 0 2.81l.34.1a1.464 1.464 0 0 1 .872 2.105l-.17.31c-.698 1.283.705 2.686 1.987 1.987l.311-.169a1.464 1.464 0 0 1 2.105.872l.1.34c.413 1.4 2.397 1.4 2.81 0l.1-.34a1.464 1.464 0 0 1 2.105-.872l.31.17c1.283.698 2.686-.705 1.987-1.987l-.169-.311a1.464 1.464 0 0 1 .872-2.105l.34-.1c1.4-.413 1.4-2.397 0-2.81l-.34-.1a1.464 1.464 0 0 1-.872-2.105l.17-.31c.698-1.283-.705-2.686-1.987-1.987l-.311.169a1.464 1.464 0 0 1-2.105-.872l-.1-.34zM8 10.93a2.929 2.929 0 1 0 0-5.86 2.929 2.929 0 0 0 0 5.858z"/>
                         </svg>
                         <br></br>
                         Settings
-                    </a>
+                    </Link>
                 </p>
 
             </Col>
